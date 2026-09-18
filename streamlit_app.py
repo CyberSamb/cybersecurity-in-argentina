19423f52785f778c44d1a116f9dd622ff3a89381
Samuel Bourgez <samuel.bz@protonmail.com>
2026-09-18 15:26:19 -0300

    Update subtitle to main title in streamlit app

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 
 st.title("La Ciberseguridad Invisibe")
-st.subtitle("La exposición de la que nadie habla")
+st.title("La exposición de la que nadie habla")
 st.write(
     "For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
